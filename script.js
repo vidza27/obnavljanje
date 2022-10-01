5dd39bf69826b4a3e566fdc18fd597c7941962b2
vidza27 <103574891+vidza27@users.noreply.github.com>
2022-10-01 15:45:21 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+var buttonElement = document.getElementById('dugme')
+
+buttonElement.addEventListener('click',function(){
+    alert('Kliknuto je dugme!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!');
+
+    for(i = 0;i<3;i++){
+        alert('broj' +i);
+    }
+});
